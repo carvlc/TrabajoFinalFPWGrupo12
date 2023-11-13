58ea8bcdd40bf1e1863ba9e76afb3fb9ba12f7e0
Joaset <jtf1wft@gmail.com>
2023-11-13 08:51:18 -0300

    Agregando un footer y estilos al home

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import ErrorPage from "./components/ErrorPage/ErrorPage";
 import AboutUs from "./components/AboutUs/AboutUs";
-import AppComparadorPrecios from "./components/ComparadorPrecios/AppCompradorPrecios";
-import AppJuegoDude from "./components/JuegoDude/AppJuegoDude";
-import AppJuegoNave from "./components/JuegoNave/AppJuegoNave";
-import AppListaDeTareas from "./components/ListaDeTareas/AppListaDeTareas";
-import AppJuegoImagenes from "./components/JuegoImagenes/AppJuegoImagenes";
+import Footer from "./components/Footer/Footer";
+// import AppComparadorPrecios from "./components/ComparadorPrecios/AppCompradorPrecios";
+// import AppJuegoDude from "./components/JuegoDude/AppJuegoDude";
+// import AppJuegoNave from "./components/JuegoNave/AppJuegoNave";
+// import AppListaDeTareas from "./components/ListaDeTareas/AppListaDeTareas";
+// import AppJuegoImagenes from "./components/JuegoImagenes/AppJuegoImagenes";
+import "./app.css";
 
 
 function App(){
@@ -16,14 +18,15 @@ function App(){
             <Header></Header>
             <Routes>
                 <Route path="/" element={<Home></Home>}></Route>
-                <Route path="/Comparador" element={<AppComparadorPrecios></AppComparadorPrecios>}></Route>
+                {/* <Route path="/Comparador" element={<AppComparadorPrecios></AppComparadorPrecios>}></Route>
                 <Route path="/Dude" element={<AppJuegoDude></AppJuegoDude>}></Route>
                 <Route path="/Nave" element={<AppJuegoNave></AppJuegoNave>}></Route>
                 <Route path="/Tareas" element={<AppListaDeTareas></AppListaDeTareas>}></Route>
-                <Route path="/JuegoImagenes" element={<AppJuegoImagenes></AppJuegoImagenes>}></Route>
+                <Route path="/JuegoImagenes" element={<AppJuegoImagenes></AppJuegoImagenes>}></Route> */}
                 <Route path="/AboutUs" element={<AboutUs></AboutUs>}></Route>
                 <Route path="*" element={<ErrorPage></ErrorPage>}></Route>
             </Routes>
+            <Footer></Footer>
         </Router>
     )
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,41 @@
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import './home.css'
+
 function Home(){
     return(
-        <div style={{padding: 20}}>
-            <h1>Grupo 12</h1>
-            <h2>este es el inicio de nuestro sitio</h2>
+        <div className='home'>
+            <div style={{ padding: 20 }}>
+                <h1>Grupo 12 FPW</h1>
+                <h2>Hola somos el grupo 12 y este es nuestro trabajo final, esperamos que les guste!!</h2>
+            </div>
+            <h2>Proyectos</h2>
+            <Container>
+                <Row xs={1} md={2} className="g-4">
+                {Array.from({ length: 4 }).map((_, idx) => (
+                    <Col key={idx}>
+                        <Card>
+                            <Card.Img variant="top" src="holder.js/100px160" />
+                            <Card.Body>
+                                <Card.Title>Card title</Card.Title>
+                                <Card.Text>
+                                    This is a longer card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit
+                                    longer.
+                                </Card.Text>
+                                <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+                </Row>
+            </Container>
+            <div>
+                <h2>AboutUs</h2>
+            </div>
         </div>
     )
 }
